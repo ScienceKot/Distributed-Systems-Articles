c5cdd7e1a448829860bee78948c5412ab940ef6d
ScienceKot <56827517+ScienceKot@users.noreply.github.com>
2023-07-03 09:43:28 +0300

    Add files via upload

---
+++ 4. Distributed Data Store/service-1/crud.py
@@ -0,0 +1,143 @@
+# Importing all needed modules.
+import uuid
+import requests
+
+
+class CRUDUser:
+    def __init__(self, leader : bool, followers : dict =None):
+        '''
+            The constructor of the CrudUser.
+        :param leader: bool
+            The parameter deciding if the service is a leader or not.
+        :param followers: dict, default = None
+            The dictionary containing the credentials of the services.
+        '''
+        self.users = {}
+        self.leader = leader
+        if self.leader:
+            self.followers = followers
+
+    def create(self, user_dict : dict):
+        '''
+            The create function from CRUD, This function adds a service to the datstore.
+        :param user_dict: dict
+            The dictionary containing the information about users.
+        :return: dict, int
+        '   The response and the status code.
+        '''
+        # If the service is a leader the service adds the user to the data store.
+        if self.leader:
+            index = str(uuid.uuid4())
+            user_dict["id"] = index
+        if user_dict["id"] not in self.users:
+            # If the service is a follower the service and the user is not registered
+            # the user is added to the data store.
+            self.users[user_dict["id"]] = user_dict
+
+            # If the service is a leader then forwards the request to the followers.
+            if self.leader:
+                for follower in self.followers:
+                    requests.post(f"http://{follower['host']}:{follower['port']}/user",
+                                  json = user_dict,
+                                  headers = {"Token" : "Leader"})
+
+            # Returning the response.
+            return user_dict, 200
+        else:
+            return {
+                "message" : "Error: User already exists!"
+            }, 400
+
+    def get_user(self, index : str):
+        '''
+            This function returns a user by the index from the data store..
+        :param index: str
+            The index of the requested user.
+        :return: dict, int
+        '   The response and the status code.
+        '''
+        # Return the user's information if it exists.
+        if index in self.users:
+            return self.users[index], 200
+        else:
+            # Return the error message if user doesn't exists.
+            return {
+                "message" : "Missing user!"
+            }, 404
+
+    def get_users(self, user_id_list : list):
+        '''
+            This function returns a list of users from the data store.
+        :param user_id_list: list
+            The list with user ids.
+        :return: dict, int
+        '   The response and the status code.
+        '''
+        # Fining the set of missing ids from the list.
+        missing_ids = set(user_id_list).difference(list(self.users.keys()))
+
+        # If there are missing ids then an error code is returned.
+        if len(missing_ids) > 0:
+            return {
+                "message" : "Missing ids!",
+                "ids" : list(missing_ids)
+            }, 404
+        else:
+            # If all ids are present in the data store a list with user credentials are returned.
+            user_info = {index : self.users[index] for index in user_id_list}
+            return user_info, 200
+
+    def update_user(self, index : str, user_dict : dict):
+        '''
+            This function updates the user's information with the provided information.
+        :param index: str
+            The index of the user in the data store.
+        :param user_dict: dict
+            The new users information.
+        :return: dict, int
+        '   The response and the status code.
+        '''
+        # Checking if the user id is registered.
+        if index in self.users:
+            # Updating the user's information.
+            self.users[index].update(user_dict)
+
+            # If the service is the leader the it forward the information to the followers.
+            if self.leader:
+                for follower in self.followers:
+                    requests.put(f"http://{follower['host']}:{follower['port']}/user/{index}",
+                                 json = self.users[index],
+                                 headers = {"Token" : "Leader"})
+
+            # Returning the response and the status code.
+            return self.users[index], 200
+        else:
+            return {
+                "message" : "Missing user!"
+            }, 404
+
+    def delete_user(self, index : str):
+        '''
+            This function deletes a user from the data store by index.
+        :param index: str
+            The index of the user in the data store.
+        :return: dict, int
+        '   The response and the status code.
+        '''
+        # Checking if the user id is registered.
+        if index in self.users:
+            # Deleting the user from the data store..
+            user_dict = self.users.pop(index)
+
+            # If the service is the leader the it forward the request to the followers.
+            if self.leader:
+                for follower in self.followers:
+                    requests.delete(f"https://{follower['host']}:{follower['port']}/user/{index}",
+                                    headers = {"Token" : "Leader"})
+
+            # Returning the response and the status code.
+            return user_dict, 200
+        else:
+            return {
+                "message" : "Missing user!"
+            }, 404
---
+++ 4. Distributed Data Store/service-2/main.py
@@ -0,0 +1,98 @@
+# Importing all needed modules.
+from flask import Flask, request
+from crud import CRUDUser
+
+# Defining the service information.
+service_info = {
+    "host" : "127.0.0.1",
+    "port" : 8001,
+    "leader" : False
+}
+
+# Creating the data store.
+crud = CRUDUser(service_info["leader"])
+
+# Creating the flask application.
+app = Flask(__name__)
+
+
+@app.route("/user", methods = ["POST"])
+def add_user():
+    '''
+        This function handles the create requests.
+    '''
+    # Extracting the headers and checking if request can be processes.
+    headers = dict(request.headers)
+    if not crud.leader and ("Token" not in headers or headers["Token"] != "leader"):
+        return {
+                   "message" : "Access denied!"
+               }, 403
+    else:
+        # Trying to create a user in the data store.
+        return_dict, status_code = crud.create(request.json)
+        return return_dict, status_code
+
+
+@app.route("/user", methods=["GET"])
+def get_users():
+    '''
+        This function handles the read some requests.
+    '''
+    # Getting the users ids.
+    user_list = request.json["users_id"]
+    # Getting the response and status code from the tha data store.
+    return_dict, status_code = crud.get_users(user_list)
+    return return_dict, status_code
+
+
+@app.route("/user/<index>", methods=["GET"])
+def get_user(index):
+    '''
+        THis function handles the read one requests.
+    '''
+    # Getting the response and the status code from the data store.
+    return_dict, status_code = crud.get_user(index)
+    return return_dict, status_code
+
+
+@app.route("/user/<index>", methods=["PUT"])
+def update_user(index):
+    '''
+        This function handles the update requests.
+    '''
+    # Extracting the headers and checking if request can be processes.
+    headers = dict(request.headers)
+    if not crud.leader and ("Token" not in headers or headers["Token"] != "leader"):
+        return {
+                   "message" : "Access denied!"
+               }, 403
+    else:
+        # Getting the new credentials from the request.
+        new_user_data = request.json
+        # Trying to update the user's information and getting the response and status code from the data store.
+        return_dict, status_code = crud.update_user(index, new_user_data)
+        return return_dict, status_code
+
+
+@app.route("/user/<index>", methods=["DELETE"])
+def delete_user(index):
+    '''
+        This function handles the delete requests.
+    '''
+    # Extracting the headers and checking if request can be processes.
+    headers = dict(request.headers)
+    if not crud.leader and ("Token" not in headers or headers["Token"] != "leader"):
+        return {
+                   "message" : "Access denied!"
+               }, 403
+    else:
+        # Trying to delete the user and getting the response and status code from data store.
+        return_dict, status_code = crud.delete_user(index)
+        return return_dict, status_code
+
+
+# Running the flask application.
+app.run(
+    host = service_info["host"],
+    port = service_info["port"]
+)
